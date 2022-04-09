32ee5b17c4d868c2cd2a6a7d923fca0f3784524e
hassanmahdi58 <hassanmahdi58@gmail.com>
2022-04-09 13:25:28 +0100

    main

--- public/js/new.js
+++ public/js/new.js
@@ -1,3 +1,30 @@
+function handlePermission() {
+  navigator.permissions.query({name:'geolocation'}).then(function(result) {
+    if (result.state == 'granted') {
+      report(result.state);
+      geoBtn.style.display = 'none';
+    } else if (result.state == 'prompt') {
+      report(result.state);
+      geoBtn.style.display = 'none';
+      navigator.geolocation.getCurrentPosition(revealPosition,positionDenied,geoSettings);
+    } else if (result.state == 'denied') {
+      report(result.state);
+      geoBtn.style.display = 'inline';
+    }
+    result.addEventListener('change', function() {
+      report(result.state);
+    });
+  });
+}
+
+function report(state) {
+  console.log('Permission ' + state);
+}
+
+handlePermission();
+
+
+
 const newFormHandler = async function(event) {
   event.preventDefault();
 
